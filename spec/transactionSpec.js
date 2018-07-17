9a4cc257509047a7daacd45e8b5224867b9653b1
Daniel Ridgway <ridginald@gmail.com>
2018-07-17 12:14:02 +0100

    implemented withdrawal date tests

--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -5,14 +5,16 @@ describe('Deposit', function() {
   var credit;
   var withdraw;
   var today;
+  var tomorrow;
   var amount;
 
   beforeEach(function() {
-    today = new Date(2012, 1, 10);
+    today = new Date(2012, 1, 13);
+    tomorrow = new Date(2012, 1, 14);
     jasmine.clock().install();
     jasmine.clock().mockDate();
     credit = new Transaction(amount, today);
-    withdraw = new Transaction(500.00);
+    withdraw = new Transaction(500.00, tomorrow);
   });
 
   afterEach(function () {
@@ -23,15 +25,19 @@ describe('Deposit', function() {
     expect(credit instanceof(Transaction)).toBe(true);
   });
 
-  it('should be able to deposit any amount', function() {
+  it('should be able to deposit any credit amount', function() {
     expect(credit.amount).toBe(amount);
   });
 
-  it('should have a deposit date', function() {
+  it('should have a credit date', function() {
     expect(credit.date).toBe(today);
   });
 
   it('should be able to withdraw correct amount(500.00)', function() {
     expect(withdraw.amount).toBe(500.00);
+  });
+
+  it('should have a withdrawal date', function() {
+    expect(withdraw.date).toBe(tomorrow);
   });
 });
